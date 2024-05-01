7df3a36df615ebbf816e03d8eee02815bf2088e1
Akash Nimbalkar <124254867+Akash-Nimbalkar@users.noreply.github.com>
2024-05-01 11:41:54 +0000

    Conversion of datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,61 @@
+let score= 33
+let score2 = "33"
+let score3 = "33abx"
+let score4 = null
+let score5 = undefined
+let score6 = ""
+
+
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+
+console.log('')
+
+console.log(typeof score2);
+let valueInNumber2 = Number(score2)
+console.log(valueInNumber2)
+console.log(typeof valueInNumber2)
+
+console.log('')
+
+console.log(typeof score3);
+let valueInNumber3 = Number(score3)
+console.log(valueInNumber3)
+console.log(typeof valueInNumber3)
+
+console.log('')
+
+console.log(typeof score4);
+let valueInNumber4 = Number(score4)
+console.log(valueInNumber4)
+console.log(typeof valueInNumber4)
+
+console.log('')
+
+console.log(typeof score5);
+let valueInNumber5 = Number(score5)
+console.log(valueInNumber5)
+console.log(typeof valueInNumber5)
+
+console.log('')
+
+console.log(typeof score6);
+let valueInNumber6 = Number(score6)
+console.log(valueInNumber6)
+console.log(typeof valueInNumber6)
+
+console.log('')
+
+console.log(typeof score6);
+let valueInNumber7 = Boolean(score6)
+console.log(valueInNumber6)
+console.log(typeof valueInNumber6)
+
+/* For converting to boolean,
+1 => true
+0 => false
+"" => false
+"JSPM" => NaN
+ */
